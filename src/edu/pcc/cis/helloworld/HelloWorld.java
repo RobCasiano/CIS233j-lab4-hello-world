34608f0d5a31768a443c6fd7af9b5082fe21a761
RobCasiano <robert.casiano@pcc.edu>
2020-10-18 16:25:50 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -8,4 +8,7 @@ package edu.pcc.cis.helloworld;
  * @version 2020.10.18
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
